ff331ac0e1241165eb8d2f0407e80c98c0891719
Ary150 <118282823+Ary150@users.noreply.github.com>
2022-11-15 15:15:30 +0100

    Add files via upload
    Micro game in Ts/Js

---
+++ Hyrule_castle_game/hyrule_castle.ts
@@ -0,0 +1,8 @@
+import { GameChoice } from "./source/functionG";
+console.log('\x1b[33m\x1b[45m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\x1b[0m\x1b[0m');
+console.log(`\x1b[33m\x1b[45m-------------------------THE LOST ELFO-------------------------\x1b[0m\x1b[0m`);
+console.log('\x1b[33m\x1b[45m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\x1b[0m\x1b[0m');
+
+console.log('\x1b[31mIl était une fois un petit lutin, se promenant dans sa forêt proche de chez lui, il trouva un trou et curieux il sauta dedans.. OP!! Plaf !! Plouf !!\n Il se retrouva dans un monde mystérieux...\n Le lutin est désormait face à un immeuble ou sur la façade était inscrit « Mairie de Busy ».\n Coincé dans ce nouveau monde il trouva Richard le clochard...\n Oh petit lutin que fais-tu ici .\n Je ne sais pas je découvre ton monde. \n Si tu veux rentrer chez toi il faudra que tu trouves le portail au dernier étage de la mairie !!\n Tiens Lutin bois ça tu en auras besoin !! \n Le Lutin saisit une canette de Guinness tendu par la main de Richard. À peine deux gorgé après le lutin senti monter lui une force extraordinaire ! \n Déterminé à rentrer chez lui, le lutin se lança avec son arme Pelindryl(une hache minuscule) à l’assaut des différents étages de la mairie!! \x1b[0m');
+
+GameChoice();
---
+++ Hyrule_castle_game/level_and_experience.ts
@@ -0,0 +1,126 @@
+import { clear, Console } from 'console';
+import { parseJsonFile,beginGameEnemy,freeFight,getRandomRarity,beginGameHero,freeHeal,menuQuestion } from './source/functionG';
+import { EnemyJson, Enemy, Hero } from './source/objectChara';
+
+const esl = require('fs');
+const readline = require('readline-sync');
+// --------------------------------------------------------------------------------
+export function randomXp(){
+  return Math.floor(Math.random() * 35);  
+}
+
+export function modLevelAndXP(){
+    let i = 1;
+    let y = 0;
+    let coin = 12;
+    let xpPlayer = 0;
+    let levelPlayer = 0;
+    const enemy1 = parseJsonFile('tab', './file_json/enemies.json');
+    const boss1 = parseJsonFile('tab', './file_json/bosses.json');
+    const player1 = parseJsonFile('tab', './file_json/players.json');
+    let enemyRandom1 :EnemyJson = getRandomRarity(enemy1);
+  const bosseRandom1 : EnemyJson = getRandomRarity(boss1);
+  const playerRandom1 : EnemyJson = getRandomRarity(player1);
+  console.clear();
+  console.log('\x1b[33m\x1b[45m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\x1b[0m\x1b[0m');
+  console.log(`\x1b[33m\x1b[45m-------------------------THE LOST ELFO-------------------------\x1b[0m\x1b[0m`);
+  console.log('\x1b[33m\x1b[45m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\x1b[0m\x1b[0m');
+
+ 
+    const answerModMenu = menuQuestion(['\x1b[31m Normal \x1b[0m', '\x1b[32m Difficile \x1b[0m', '\x1b[32m Folie Furieuse\x1b[0m']);
+    const answerNbLevelMenu = menuQuestion(['\x1b[31m 10 \x1b[0m', '\x1b[32m 20 \x1b[0m','\x1b[32m 50 \x1b[0m', '\x1b[32m 100 \x1b[0m']);
+
+    if (answerModMenu === 1) {
+      enemyRandom1.hp *= (3 / 2);
+      enemyRandom1.str *= (3 / 2);
+      bosseRandom1.hp *= (3 / 2);
+      bosseRandom1.str *= (3 / 2);
+    }
+    if (answerModMenu === 2) {
+      enemyRandom1.hp *= (2);
+      enemyRandom1.str *= (2);
+      bosseRandom1.hp *= (2);
+      bosseRandom1.str *= (2);
+    }
+    if(answerNbLevelMenu === 0){
+        y = 10;
+    }
+    if (answerNbLevelMenu === 1) {
+      y = 20;
+    }
+    if (answerNbLevelMenu === 2) {
+      y = 50;
+    }
+    if (answerNbLevelMenu === 3) {
+      y = 100;
+    }
+    const hpBeforeGame = playerRandom1.hp;
+    const enemyRandom2HP = enemyRandom1.hp;
+
+    console.clear();
+    console.log('\x1b[33m\x1b[45m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\x1b[0m\x1b[0m');
+    console.log(`\x1b[33m\x1b[45m-------------------------THE LOST ELFO-------------------------\x1b[0m\x1b[0m`);
+    console.log('\x1b[33m\x1b[45m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\x1b[0m\x1b[0m');
+
+
+    while ((playerRandom1.hp >= 0 || enemyRandom1.hp >= 0) && i < y) {
+        console.log(`\x1b[31m\x1b[47m ====== Niveau ${i} ====== \x1b[0m\x1b[0m`);
+        console.log('Tu as : '+coin+' coin !!');
+        console.log('Tu as : '+levelPlayer+' de level :D');
+        console.log('Tu as : '+xpPlayer+' d\' XP ;)');
+        console.log('Nouvel Ennemi!');
+        console.log(`Ses Stats:`);
+        console.log(`Nom: ${enemyRandom1.name}`);
+        console.log(`Force: ${enemyRandom1.str}`);
+        console.log(`\x1b[32mTes PV : ${playerRandom1.hp}\x1b[0m`);
+        const answer = menuQuestion(['\x1b[31m MACHETTE \x1b[0m', '\x1b[32m GUINESS \x1b[0m']);
+
+      if (answer === 0) {
+        freeFight(enemyRandom1, playerRandom1);
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+      }
+      if (answer === 1 && playerRandom1.hp < hpBeforeGame) {
+        freeHeal(enemyRandom1, playerRandom1, hpBeforeGame);
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+      } if (answer === 1 && playerRandom1.hp >= hpBeforeGame) {
+        console.log('\x1b[1mTa santé est pleine!\x1b[0m');
+      }
+      if (answer === -1) {
+        console.log('\x1b[33mDommage, reviens quand tu seras plus fort...\x1b[0m');
+        console.clear();
+        break;
+      }
+      if (playerRandom1.hp <= 0) {
+        console.log(`\x1b[33mTu n'es pas l'élu. Adieu.\x1b[0m`);
+        break;
+      }
+      if (enemyRandom1.hp <= 0) {
+        console.log('Tu te débrouilles bien petit Lutin');
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        i += 1;
+        coin +=1;
+        xpPlayer += randomXp();
+       
+        if(xpPlayer > 10 || xpPlayer > 20 || xpPlayer > 25 || xpPlayer > 30 || xpPlayer > 35){
+          levelPlayer+=1;
+          playerRandom1.hp = playerRandom1.hp*(3 / 2);
+          playerRandom1.str = playerRandom1.str*(3 / 2)
+        }
+        enemyRandom1.hp = enemyRandom2HP;
+        console.log('-----------------------------------------------------------------------------------------------');
+        console.clear();
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+     
+      if (i === 10 || i === 20 || i === 30 || i === 40 || i === 50 || i === 60 || i === 70 || i === 80 ||i === 90 || i ===100) {
+        enemyRandom1 = bosseRandom1;
+        console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
+        console.log(`Dernier étage mon Lutin! Tu affrontes ${bosseRandom1.name}, de force égale à ${bosseRandom1.str}`);
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+    }
+ 
+}
