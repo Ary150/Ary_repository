736896a56f5ca1c24eae2bfeb7c6310c275f5387
Ary150 <118282823+Ary150@users.noreply.github.com>
2022-11-15 15:22:42 +0100

    Add files via upload
    Pdo_bdd

---
+++ pdo_xml/test_pdo_php.php
@@ -0,0 +1,28 @@
+<?php
+try
+{
+	// On se connecte à MySQL
+	$mysqlClient = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'ares', 'mppdo');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table recipes
+$sqlQuery = 'SELECT * FROM pokemon limit 10';
+$recipesStatement = $mysqlClient->prepare($sqlQuery);
+$recipesStatement->execute();
+$recipes = $recipesStatement->fetchAll();
+
+// On affiche chaque recette une à une
+foreach ($recipes as $recipe) {
+?>
+    <p><?php echo $recipe['author']; ?></p>
+<?php
+}
+?>
+
